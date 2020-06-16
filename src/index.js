eeb47eb35a06e32ac1153af6fef4c11ec4b12f2e
Avanesov Philipp <floppyrip@gmail.com>
2020-06-16 17:14:38 +0300

    init

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class Tooltip extends React.Component {
+    render() {
+        return <h1>this is tooltip</h1>
+    }
+}
+
+export default Tooltip;
