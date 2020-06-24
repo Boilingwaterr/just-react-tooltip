2b84a174c721ed21f974b41a2015374d9420e4d4
Avanesov Philipp <floppyrip@gmail.com>
2020-06-25 01:41:20 +0300

    stuffs

--- doczrc.js
+++ doczrc.js
@@ -1,3 +1,9 @@
-export default {
+module.exports = {
+    port: process.env.PORT || 3000,
     base: '/docz',
+    dest: "/docz/documentation",
+    plugins: [
+        `gatsby-alias-imports`,
+        `gatsby-plugin-root-import`,
+    ]
 }
